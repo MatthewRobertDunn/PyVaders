431d7cb1718c5f387a95d4319f739af15cf50896
Matthew <mrdunn@gmail.com>
2021-11-17 15:30:38 +1030

    Added fun spinny earth background

--- entities/alien.py
+++ entities/alien.py
@@ -4,7 +4,7 @@ from entities.takesdamage_trait import TakesDamageTrait
 class Alien(PhysicsTrait, TakesDamageTrait):
     #Create the physics component of the game entity.
     def create_physics_body(self, position):
-        self.create_kinematic_rectangle_body(position, 2.0, 1.9) 
+        self.create_kinematic_rectangle_body(position, 2.0, 2.9) 
 
     #Create graphics component
     def create_graphics_model(self):
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from panda3d.core import *
 from panda3d.core import ConfigVariableString
+from panda3d.core import DrawMask, BitArray
 coord_system = ConfigVariableString("coordinate-system")
 coord_system.setValue("yup-right")
 
@@ -25,16 +26,45 @@ class MyApp(ShowBase):
         lens.setFilmSize(80*0.8, 60*0.8)  # Or whatever is appropriate for your scene
         base.cam.node().setLens(lens)
         base.cam.setPos(0,0,0)
+        
+        self.taskMgr.add(self.physics_task, "physics",None,None,-100)
+        self.context = InvaderContext(self.keys, self.loader)
+        
+        self.rootNode = self.render.attachNewNode("rootNode")
+        self.backgroundNode = self.rootNode.attachNewNode("backgroundRoot")
+        self.threeDNode = self.rootNode.attachNewNode("2dstuff")
+        self.backgroundCamera = base.makeCamera(base.win,camName="camBack")
+        self.backgroundCamera.reparentTo(self.backgroundNode)
+        cm = CardMaker("cm")
+        self.cardNode = self.loader.loadModel("gfx/scene.gltf")
+        self.cardNode.setScale(0.01)
+        self.cardNode.setPos(0, 0, -100)
+        self.cardNode.reparentTo(self.backgroundNode)
+        base.camera.reparentTo(self.threeDNode)
+        self.context.render_node.reparentTo(self.threeDNode)
+        self.backgroundCamera.node().setCameraMask(1)
+        self.cam.node().setCameraMask(2)
+        self.hideFromCamera(self.backgroundCamera, self.threeDNode)
+        self.hideFromCamera(self.cam, self.backgroundNode)
+
         dlight = DirectionalLight('directionalLight')
         dlight.setDirection(Vec3(0, 0, -1)) # (towards right-back-bottom; should only illuminate front/left/top )
         dlight.setColor(Vec4(1, 1, 1, 1))
-        dlightNP = render.attachNewNode(dlight)
-        render.setLight(dlightNP)
-        self.taskMgr.add(self.physics_task, "physics",None,None,-100)
-        self.context = InvaderContext(self.keys, self.loader)
-        self.context.render_node.reparentTo(self.render)
+        dlightNP = self.threeDNode.attachNewNode(dlight)
+        self.threeDNode.setLight(dlightNP)
+        
+
+
+    def hideFromCamera(self,camera1,nodePath):
+        camera1Bits = BitArray()
+        camera1Bits.setWord(0,camera1.node().getCameraMask().getWord())
+        showMask =~(camera1Bits)
+        hideMask = (camera1Bits)
+        clearMask = BitArray.allOff()
+        nodePath.node().adjustDrawMask(BitMask32(showMask.getWord(0).getWord()),hideMask.getWord(0).getWord(),clearMask.getWord(0).getWord())
 
     def physics_task(self, task):
+        self.cardNode.setHpr(task.time,0,0)
         dt = round(globalClock.getDt(),4)
         self.keys.poll(base.mouseWatcherNode)
         self.context.tick(task.time,dt)
